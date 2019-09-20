e9aadaaca963f62b0ccb8584b5cbc6e2ddd14f1f
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2019-09-20 14:05:30 -0700

    added README / modified setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Heterogeneous synaptic weighting improves neural coding in the presence of common noise
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='noise_diversity',
+    name='nd',
     description='',
     long_description=long_description,
     author='Pratik Sachdeva',
